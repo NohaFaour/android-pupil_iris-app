d8e4ade1c4147c28431fc4a1269ba4d36e913165
NohaFaour <nmf21@mail.aub.edu>
2025-06-09 14:39:29 +0300

    final apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("my-release-key.jks")
+            storePassword = "iris_android"
+            keyAlias = "app_alias"              // <-- this must match
+            keyPassword = "iris_android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -25,6 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
